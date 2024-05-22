e7c676e96891e2f2ece41d470bad9fde8793e6ef
Emrah Kahraman <oempire34@gmail.com>
2024-05-23 00:33:09 +0300

    update-12

--- src/templates/ServicesClients.js
+++ src/templates/ServicesClients.js
@@ -12,107 +12,105 @@ function ServicesClients() {
     <>
       <div id="services_clients">
         <div className="container">
-          <Swiper
-            className="services_clients"
-            modules={[Autoplay]}
-            spaceBetween={0}
-            slidesPerView={2}
-            loop={true}
-            autoplay={{
-              delay: 2500,
-              pauseOnMouseEnter: true,
-              disableOnInteraction: false
-            }}
-          >
-            <SwiperSlide className="services_clients_item">
-              <p>
-                Teritatis et quasi architecto. Sed ut perspiciatis unde omnis
-                iste natus error sit voluptatem accusantium dolore mque
-                laudantium.
-              </p>
-              <div className="client_desc">
-                <div className="client_desc_image">
-                  <Image
-                    className="img_fluid"
-                    src={ClientImage}
-                    alt="Client Image"
-                  />
+          <div className="services_clients">
+            <div className="services_clients_heading">
+              <h6>
+                <span>Client</span> says
+              </h6>
+            </div>
+            {/*services_clients_heading*/}
+            <Swiper
+              className="services_clients_boxs"
+              modules={[Autoplay]}
+              spaceBetween={30}
+              slidesPerView={2}
+              loop={true}
+              autoplay={{
+                delay: 3000,
+                pauseOnMouseEnter: true,
+                disableOnInteraction: false
+              }}
+              breakpoints={{
+                0: {
+                  slidesPerView: 1
+                },
+                768: {
+                  slidesPerView: 2
+                },
+                992: {
+                  slidesPerView: 2
+                }
+              }}
+            >
+              <SwiperSlide className="services_clients_boxs_item">
+                <p>
+                  Teritatis et quasi architecto. Sed ut perspiciatis unde omnis
+                  iste natus error sit voluptatem accusantium dolore mque
+                  laudantium.
+                </p>
+                <div className="client_desc">
+                  <div className="client_desc_image">
+                    <Image
+                      className="img_fluid"
+                      src={ClientImage}
+                      alt="Client Image"
+                    />
+                  </div>
+                  <div className="client_desc_text">
+                    <h5>Rudhi Sasmito</h5>
+                    <h6>ABC Company</h6>
+                  </div>
                 </div>
-                <div className="client_desc_text">
-                  <h5>Rudhi Sasmito</h5>
-                  <h6>ABC Company</h6>
+                {/*client_desc*/}
+              </SwiperSlide>
+              {/*services_clients_boxs_item*/}
+              <SwiperSlide className="services_clients_boxs_item">
+                <p>
+                  Teritatis et quasi architecto. Sed ut perspiciatis unde omnis
+                  iste natus error sit voluptatem accusantium dolore mque
+                  laudantium.
+                </p>
+                <div className="client_desc">
+                  <div className="client_desc_image">
+                    <Image
+                      className="img_fluid"
+                      src={ClientImage}
+                      alt="Client Image"
+                    />
+                  </div>
+                  <div className="client_desc_text">
+                    <h5>Rudhi Sasmito</h5>
+                    <h6>ABC Company</h6>
+                  </div>
                 </div>
-              </div>
-              {/*client_desc*/}
-            </SwiperSlide>
-            {/*services_clients_item*/}
-            <SwiperSlide className="services_clients_item">
-              <p>
-                Teritatis et quasi architecto. Sed ut perspiciatis unde omnis
-                iste natus error sit voluptatem accusantium dolore mque
-                laudantium.
-              </p>
-              <div className="client_desc">
-                <div className="client_desc_image">
-                  <Image
-                    className="img_fluid"
-                    src={ClientImage}
-                    alt="Client Image"
-                  />
+                {/*client_desc*/}
+              </SwiperSlide>
+              {/*services_clients_boxs_item*/}
+              <SwiperSlide className="services_clients_boxs_item">
+                <p>
+                  Teritatis et quasi architecto. Sed ut perspiciatis unde omnis
+                  iste natus error sit voluptatem accusantium dolore mque
+                  laudantium.
+                </p>
+                <div className="client_desc">
+                  <div className="client_desc_image">
+                    <Image
+                      className="img_fluid"
+                      src={ClientImage}
+                      alt="Client Image"
+                    />
+                  </div>
+                  <div className="client_desc_text">
+                    <h5>Rudhi Sasmito</h5>
+                    <h6>ABC Company</h6>
+                  </div>
                 </div>
-                <div className="client_desc_text">
-                  <h5>Rudhi Sasmito</h5>
-                  <h6>ABC Company</h6>
-                </div>
-              </div>
-              {/*client_desc*/}
-            </SwiperSlide>
-            {/*services_clients_item*/}
-            <SwiperSlide className="services_clients_item">
-              <p>
-                Teritatis et quasi architecto. Sed ut perspiciatis unde omnis
-                iste natus error sit voluptatem accusantium dolore mque
-                laudantium.
-              </p>
-              <div className="client_desc">
-                <div className="client_desc_image">
-                  <Image
-                    className="img_fluid"
-                    src={ClientImage}
-                    alt="Client Image"
-                  />
-                </div>
-                <div className="client_desc_text">
-                  <h5>Rudhi Sasmito</h5>
-                  <h6>ABC Company</h6>
-                </div>
-              </div>
-              {/*client_desc*/}
-            </SwiperSlide>
-            {/*services_clients_item*/}
-            <SwiperSlide className="services_clients_item">
-              <p>
-                Teritatis et quasi architecto. Sed ut perspiciatis unde omnis
-                iste natus error sit voluptatem accusantium dolore mque
-                laudantium.
-              </p>
-              <div className="client_desc">
-                <div className="client_desc_image">
-                  <Image
-                    className="img_fluid"
-                    src={ClientImage}
-                    alt="Client Image"
-                  />
-                </div>
-                <div className="client_desc_text">
-                  <h5>Rudhi Sasmito</h5>
-                  <h6>ABC Company</h6>
-                </div>
-              </div>
-              {/*client_desc*/}
-            </SwiperSlide>
-            {/*services_clients_item*/}
-          </Swiper>
+                {/*client_desc*/}
+              </SwiperSlide>
+              {/*services_clients_boxs_item*/}
+            </Swiper>
+            {/*services_clients_boxs*/}
+          </div>
           {/*services_clients*/}
         </div>
         {/*container*/}
--- src/templates/ServicesPricings.js
+++ src/templates/ServicesPricings.js
@@ -12,29 +12,81 @@ function ServicesPricings() {
               </h6>
             </div>
             {/*services_pricings_heading*/}
-            <div className="services_pricings_items">
-              <div className="services_pricings_items_box">
-                <h4>POPULAR</h4>
-                <h5>Professional</h5>
-                <h6>$9.99/mo</h6>
-                <p>for up to 2 editors and 3 projects</p>
-                <ul>
-                  <li>
-                    <Link href="/">3 projects</Link>
-                  </li>
-                  <li>
-                    <Link href="/">30-day version history</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Up to 2 editors</Link>
-                  </li>
-                  <li>
-                    <Link href="/">Unlimited cloud storage</Link>
-                  </li>
-                </ul>
-                <button>
-                  <Link href="/">Get Now</Link>
-                </button>
+            <div className="services_pricings_items row">
+              <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="services_pricings_items_box">
+                  <h5>Professional</h5>
+                  <h6>$9.99/mo</h6>
+                  <p>for up to 2 editors and 3 projects</p>
+                  <ul>
+                    <li>
+                      <Link href="/">3 projects</Link>
+                    </li>
+                    <li>
+                      <Link href="/">30-day version history</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Up to 2 editors</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Unlimited cloud storage</Link>
+                    </li>
+                  </ul>
+                  <button>
+                    <Link href="/">Get Now</Link>
+                  </button>
+                </div>
+              </div>
+              {/*services_pricings_items_box*/}
+              <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="services_pricings_items_box">
+                  <h4>POPULAR</h4>
+                  <h5>Professional</h5>
+                  <h6>$9.99/mo</h6>
+                  <p>for up to 2 editors and 3 projects</p>
+                  <ul>
+                    <li>
+                      <Link href="/">3 projects</Link>
+                    </li>
+                    <li>
+                      <Link href="/">30-day version history</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Up to 2 editors</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Unlimited cloud storage</Link>
+                    </li>
+                  </ul>
+                  <button>
+                    <Link href="/">Get Now</Link>
+                  </button>
+                </div>
+              </div>
+              {/*services_pricings_items_box*/}
+              <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="services_pricings_items_box">
+                  <h5>Professional</h5>
+                  <h6>$9.99/mo</h6>
+                  <p>for up to 2 editors and 3 projects</p>
+                  <ul>
+                    <li>
+                      <Link href="/">3 projects</Link>
+                    </li>
+                    <li>
+                      <Link href="/">30-day version history</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Up to 2 editors</Link>
+                    </li>
+                    <li>
+                      <Link href="/">Unlimited cloud storage</Link>
+                    </li>
+                  </ul>
+                  <button>
+                    <Link href="/">Get Now</Link>
+                  </button>
+                </div>
               </div>
               {/*services_pricings_items_box*/}
             </div>
